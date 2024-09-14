9fa07945e9313e4dfe15726aa92868f8a7fedfcb
Anand Paramaje <anandparmaje@gmail.com>
2024-09-14 15:51:06 +0530

    First_Code

--- src/java/Demo.java
+++ src/java/Demo.java
@@ -1,7 +1,19 @@
 public class Demo{
         public static void main(String[] args) {
-                String A1="Hello World";
-                System.out.println()
+                String A1 = "Hello World";
+                String A2 = "anandparamaje@gmail.com";
+
+                System.out.println(A1 == A2); // true
+                System.out.println(A1.toLowerCase()); // hello world
+                System.out.println(A1.toUpperCase()); // HELLO WORLD
+                System.out.println(+A1.length()); // 11
+                System.out.println(A1.concat(" Morning")); //Hello World Morning
+                System.out.println(A1.charAt(0)); //H
+                System.out.println(A1.equals("Hello World")); //true
+                System.out.println(A1.contains("W")); // true
+                System.out.println(A1.indexOf('o')); //4
+                System.out.println(A1.replaceAll("World","Guys")); // Hello Guys
+
         }
 
 
