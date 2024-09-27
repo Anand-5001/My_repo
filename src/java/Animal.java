331fba53832429ac442ff07d805392d9bc3aeaed
Anand Paramaje <anandparmaje@gmail.com>
2024-09-27 08:28:25 +0530

    feat(56):ZooHierarchy

---
+++ src/java/Animal.java
@@ -0,0 +1,6 @@
+public class Animal {
+
+    public void sound(){
+        System.out.println("The Sounds of Some Animals are as follows");
+    }
+}
---
+++ src/java/MainZoo.java
@@ -0,0 +1,16 @@
+public class MainZoo {
+
+    public static void main(String[] args){
+        Animal a=new Animal();
+        a.sound();
+
+        Dog d=new Dog();
+        d.sound();
+
+        Cat c=new Cat();
+        c.sound();
+
+        Lion l=new Lion();
+        l.sound();
+    }
+}
